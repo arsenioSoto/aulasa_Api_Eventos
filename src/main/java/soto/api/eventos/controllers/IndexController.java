123823e54dedda0a5d1bd7e79c79b2e34bd4f9e3
Arsenio Soto Junior <sotosword@gmail.com>
2021-05-22 04:40:10 +1100

    Colocando os arquivos inicias da minha aplicacao

---
+++ src/main/java/soto/api/eventos/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package soto.api.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String index() {
+	System.out.println("Chamou o metodo index");
+		return "home";
+	}
+
+}
