8f4bb61fcb4b0337307235b598bca4210a7dd14f
Arsenio Soto Junior <sotosword@gmail.com>
2021-05-22 10:35:30 +1100

    Criando-a-listagem-de-eventos

--- src/main/java/soto/api/eventos/controllers/IndexController.java
+++ src/main/java/soto/api/eventos/controllers/IndexController.java
@@ -9,7 +9,7 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 	System.out.println("Chamou o metodo index");
-		return "home";
+		return "redirect:/eventos";
 	}
 
 }
